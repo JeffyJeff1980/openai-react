037d2bbf0d258d206428bca4f3c66153e0bbb0f1
JFB <neurovortex@hotmail.com>
2023-11-27 18:45:28 -0500

    iMessage colors

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -9,7 +9,7 @@ interface MessageProps {
 const Message: React.FC<MessageProps> = ({ message }) => {
   return (
     <div style={{ textAlign: message.isUser ? "right" : "left", margin: "8px" }}>
-      <div style={{ backgroundColor: message.isUser ? "#DCF8C6" : "#b8e3fc", padding: "8px", borderRadius: "8px" }}>
+      <div style={{ color: message.isUser ? "#ffffff" : "#000000", backgroundColor: message.isUser ? "#1186fe" : "#eaeaea", padding: "8px", borderRadius: "8px" }}>
         {message.content.split("\n").map((text, index) => (
           <p key={index}>{text}</p>
         ))}
